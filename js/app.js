b54b582879b2da650cb6483bfa41c04918974237
tadrochlinski <tadrochlinski@gmail.com>
2022-07-07 17:35:11 +0200

    bmi calculator done

--- js/app.js
+++ js/app.js
@@ -17,14 +17,31 @@ const onSlidersChange = (inp) =>{
         height = parseFloat(inp.value)/100;
         heightLabel.innerHTML = `${parseInt(height * 100)}cm`
     }
-
-    console.log(weight, height)
-    bmiCalc(weight, height);
+    
+    const {bmi, category} = bmiCalc(weight, height);
+    output.innerHTML = `${bmi.toPrecision(3)} : ${category}`
 }
 
 const bmiCalc = (weight, height) =>{
+    const scale = [
+        {name: 'underweight', min: 0, max: 18.5},
+        {name: 'normal', min: 18.5, max: 24.9},
+        {name: 'overweight', min: 25, max: 29.9},
+        {name: 'obese', min: 30, max:34.9},
+        {name: 'extremly obese', min: 35, max: 200},
+    ];
+
     const bmi = weight/(height*height);
-    console.log(bmi);
+    let weightCat;
+    scale.forEach((cat , i)=>{
+        const {name, min, max} = cat;
+        if(bmi > min && bmi < max) weightCat = name;
+    });
+
+    const userInfo = {bmi: bmi, category: weightCat};
+
+
+    return userInfo;
 }
 
 rangeInps.forEach((inp) => inp.addEventListener('input', () => onSlidersChange(inp)));
